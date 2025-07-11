5e2121a94ac4be032db7ccf29836c17db8edcd9b
some3L <luckyllemos0909@gmail.com>
2025-07-11 16:54:26 +0800

    Update adjust onClick action into whole image

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,6 +9,7 @@ import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({ name, description, tags, image, source_code_link }) => {
+
   return (
     <motion.div variants={fadeIn("up", "spring")}>
       <Tilt
@@ -19,7 +20,8 @@ const ProjectCard = ({ name, description, tags, image, source_code_link }) => {
         }}
         className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
-        <div className="relative w-full h-[230px]">
+        <div className="relative w-full h-[230px] cursor-pointer"
+          onClick={() => window.open(source_code_link, "_blank")}>
           <img
             src={image}
             alt="project_image"
@@ -27,8 +29,7 @@ const ProjectCard = ({ name, description, tags, image, source_code_link }) => {
           />
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center"
             >
               <img src={github} alt="source code" className="w-1/2 h-1/2 object-contain" />
             </div>
