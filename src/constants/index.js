5d9cdbdc713cf8498b1d43bbc9dde557315aab74
some3L <luckyllemos0909@gmail.com>
2025-07-26 10:58:17 +0800

    Update add new projects with details

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,7 @@ import {
   git,
   gcm3,
   tsukiden,
-  reloved,
+  relovedCompany,
   vuejs,
   nuxtjs,
   mongoDB,
@@ -20,7 +20,9 @@ import {
   mysql,
   aws,
   mclaudTechnology,
-  mclaudTechnologyCompany
+  mclaudTechnologyCompany,
+  reloved,
+  talentSage
 } from "../assets";
 
 export const navLinks = [
@@ -104,7 +106,7 @@ export const experiences = [
   {
     title: "Full Stack Web Developer",
     company_name: "Reloved UAE",
-    icon: reloved,
+    icon: relovedCompany,
     iconBg: "#B2B2AF",
     date: "July 2024 - Present",
     points: [
@@ -122,72 +124,40 @@ export const projects = [
     description:
       "Web-based platform that allows users to access weather information for their location by entering it in the search field",
     tags: [
-      { name: "Javascript", color: "blue-text-gradient" },
+      { name: "JavaScript", color: "blue-text-gradient" },
       { name: "HTML", color: "green-text-gradient" },
-      { name: "bootstrap 5.3.0", color: "pink-text-gradient" },
-      { name: "Weather API by API Ninjas", color: "yellow-text-gradient" },
+      { name: "CSS", color: "green-text-gradient" },
+      { name: "Liquid", color: "pink-text-gradient" },
+      { name: "Shopify", color: "pink-text-gradient" },
     ],
     image: mclaudTechnology,
     source_code_link: "https://mclaud.technology/",
   },
   {
-    name: "Terminal Like Portfolio Website",
+    name: "TalentSage",
     description:
       "A terminal themed portfolio website that allows users to type into the terminal and use commands like a real terminal.",
     tags: [
       { name: "HTML", color: "blue-text-gradient" },
-      { name: "css", color: "green-text-gradient" },
-      { name: "Javascript", color: "pink-text-gradient" },
-    ],
-    // image: termpw,
-    source_code_link: "https://github.com/lohitkolluri/lohitkolluri.github.io",
-  },
-  {
-    name: "Mental Health Fitness Tracker",
-    description:
-      "ML model that utilizes regression techniques to provide insights into mental health and make predictions based on the available data.",
-    tags: [
-      { name: "Machine Learning", color: "blue-text-gradient" },
-      { name: "Jupyter Notebook", color: "green-text-gradient" },
-      { name: "Regression Algorithms", color: "pink-text-gradient" },
-    ],
-    // image: mhft,
-    source_code_link:
-      "https://github.com/lohitkolluri/mental_health_fitness_tracker",
-  },
-  {
-    name: "PayloadMaster",
-    description:
-      "Tool to automate payload creation using the Metasploit framework",
-    tags: [
-      { name: "shell", color: "blue-text-gradient" },
-    ],
-    // image: payloadmaster,
-    source_code_link: "https://github.com/lohitkolluri/PayloadMaster",
-  },
-  {
-    name: "CompileVortex",
-    description:
-      "Tool to automate payload creation using the Metasploit framework",
-    tags: [
-      { name: "Javascript", color: "blue-text-gradient" },
       { name: "CSS", color: "green-text-gradient" },
-      { name: "HTML", color: "pink-text-gradient" },
+      { name: "Vuetify", color: "pink-text-gradient" },
+      { name: "VueJS", color: "pink-text-gradient" },
+      { name: "MEVN", color: "pink-text-gradient" },
     ],
-    // image: CompileVortex,
-    source_code_link: "https://github.com/lohitkolluri/CompileVortex",
+    image: talentSage,
+    source_code_link: "https://www.talentsage.com/",
   },
   {
-    name: "Sketcher",
+    name: "The Reloved Marketplace",
     description:
-      "Convert an input image to a pencil sketch using OpenCV and Matplotlib libraries.",
+      "Reloved is the UAE’s first eco-tech platform where you can buy and sell secondhand fashion, furniture, luxury and baby & items.",
     tags: [
-      { name: "OpenCV", color: "blue-text-gradient" },
-      { name: "Matplotlib", color: "green-text-gradient" },
-      { name: "Python", color: "pink-text-gradient" },
+      { name: "ReactJS", color: "blue-text-gradient" },
+      { name: "AWS", color: "green-text-gradient" },
+      { name: "TailwindCSS", color: "pink-text-gradient" },
     ],
-
-    // image: sketcher,
-    source_code_link: "https://github.com/lohitkolluri/Image_to_Pencil_Sketch_App",
+    image: reloved,
+    source_code_link:
+      "https://www.therelovedmarketplace.com/",
   },
 ];
